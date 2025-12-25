697eaffdb2f6d3ac175399eb6fd29d0f4a6ec8f3
hasan8651 <hasan865@gmail.com>
2025-12-25 22:37:27 +0600

    fixing email sending

--- src/lib/email.js
+++ src/lib/email.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export async function sendBookingEmail({ to, booking }) {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT || 587),
-    secure: false,
+    port: Number(process.env.EMAIL_PORT || 465),
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
